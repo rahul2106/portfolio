64b2ee7e1c69781e5e2f1c76a437a38c67862313
rahul kumarcd <rahul.kmr2106@gmail.com>
2020-12-18 16:46:30 +0000

    CodeBase

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portfolio';
+}
